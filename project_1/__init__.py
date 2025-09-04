a81077e9ffdf92ea207a1fa7345dd869109ea0df
aadi-14 <adisharma14500@gmail.com>
2025-09-04 20:21:17 +0530

    Setting up MySQL as default Database

---
+++ project_1/__init__.py
@@ -0,0 +1,4 @@
+import pymysql
+
+# Allow PyMySQL to be used as MySQLdb for Django
+pymysql.install_as_MySQLdb()
--- project_1/settings.py
+++ project_1/settings.py
@@ -74,6 +74,17 @@ WSGI_APPLICATION = 'project_1.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_todo',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
+    },
+    'sqlite3': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -104,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'  # 'UTC'
 
 USE_I18N = True
 
